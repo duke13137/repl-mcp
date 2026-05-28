d8f288eda2a6ccc225861401d313765c3a49ab40
Duke <duke@home.com>
2026-05-28 14:27:15 -0400

    update deps.edn

--- build.clj
+++ build.clj
@@ -6,6 +6,14 @@
 (def version "0.1.0-SNAPSHOT")
 (def main 'is.simm.repl-mcp)
 (def class-dir "target/classes")
+(def basis (delay (b/create-basis {:project "deps.edn"})))
+(def jar-file (format "target/%s-%s.jar" (name lib) version))
+(def uber-file (format "target/%s-%s-standalone.jar" (name lib) version))
+(def src-dirs ["src"])
+(def resource-dirs ["resources"])
+
+(defn clean "Remove build artifacts." [_]
+  (b/delete {:path "target"}))
 
 (defn test "Run all the tests." [opts]
   (let [basis    (b/create-basis {:aliases [:test]})
@@ -20,18 +28,45 @@
 (defn- uber-opts [opts]
   (assoc opts
          :lib lib :main main
-         :uber-file (format "target/%s-%s.jar" lib version)
-         :basis (b/create-basis {})
+         :uber-file uber-file
+         :basis @basis
          :class-dir class-dir
-         :src-dirs ["src"]
+         :src-dirs src-dirs
          :ns-compile [main]))
+
+(defn jar "Build the library jar." [opts]
+  (clean opts)
+  (println "\nWriting pom.xml...")
+  (b/write-pom {:class-dir class-dir
+                :lib lib
+                :version version
+                :basis @basis
+                :src-dirs src-dirs
+                :resource-dirs resource-dirs})
+  (println "\nCopying source and resources...")
+  (b/copy-dir {:src-dirs (into resource-dirs src-dirs)
+               :target-dir class-dir})
+  (println "\nBuilding JAR...")
+  (b/jar {:class-dir class-dir
+          :jar-file jar-file})
+  opts)
+
+(defn install "Install the library jar to the local Maven repository." [opts]
+  (jar opts)
+  (println "\nInstalling JAR...")
+  (b/install {:basis @basis
+              :lib lib
+              :version version
+              :jar-file jar-file
+              :class-dir class-dir})
+  opts)
 
 (defn ci "Run the CI pipeline of tests (and build the uberjar)." [opts]
   (test opts)
-  (b/delete {:path "target"})
+  (clean opts)
   (let [opts (uber-opts opts)]
     (println "\nCopying source...")
-    (b/copy-dir {:src-dirs ["resources" "src"] :target-dir class-dir})
+    (b/copy-dir {:src-dirs (into resource-dirs src-dirs) :target-dir class-dir})
     (println (str "\nCompiling " main "..."))
     (b/compile-clj opts)
     (println "\nBuilding JAR...")
